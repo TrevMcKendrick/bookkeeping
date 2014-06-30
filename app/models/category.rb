3fbea768a7a4fc491b586a7100847f4d442f5463
trevmckendrick <trevormckendrick@gmail.com>
2014-06-30 16:23:08 -0600

    Setup Category with initial constants

---
+++ app/models/category.rb
@@ -0,0 +1,5 @@
+class Category
+  REVENUE_CATEGORIES = %w[Sales Returns\ and\ Allowances]
+  EXPENSE_CATEGORIES = %w[Payroll\ Expense Auto\ expense Meals\ and\ Entertainment Travel\ Expense Bank\ Fees Rent\ Expense]
+  DEFAULTS = REVENUE_CATEGORIES + EXPENSE_CATEGORIES
+end
