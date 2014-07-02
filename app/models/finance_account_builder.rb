94abb1e82ea53013fecaccee569e50114b933aea
trevmckendrick <trevormckendrick@gmail.com>
2014-07-02 02:19:21 -0600

    add test values to account form

--- app/models/finance_account_builder.rb
+++ app/models/finance_account_builder.rb
@@ -4,17 +4,15 @@ class FinanceAccountBuilder
   attr_accessor :plaid_type, :username, :password, :email
 
   def initialize(hash)
-    @plaid_type = hash[:plaid_type]
-    @username = hash[:username]
-    @password = hash[:password]
-    @email = hash[:email]
+    @plaid_type = hash[:plaid_type] || "wells"
+    @username = hash[:username] || "plaid_test"
+    @password = hash[:password] || "plaid_good"
+    @email = hash[:email] || "test@plaid.com"
   end
 
   def as_finance_account
     account = Plaid.call.add_account(@plaid_type,@username,@password,@email)
-    binding.pry
     FinanceAccount.create(:access_token => account[:access_token])
   end
 
-  def 
 end
