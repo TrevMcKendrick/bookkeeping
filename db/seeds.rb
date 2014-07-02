7b4b697dd90650e0fecd5bfc72199c868d4ef1d3
trevmckendrick <trevormckendrick@gmail.com>
2014-07-02 00:41:06 -0600

    Seed file creates finance institutions and removes entries ONLY IN DEVELOPMENT

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+if Rails.env == "development"
+  Plutus::Entry.destroy_all
+  Plutus::Amount.destroy_all
+  Plutus::Account.destroy_all
+end
+
 Plutus::Asset.create(:name => "Cash")
 Plutus::Asset.create(:name => "Accounts Receivable")
 Plutus::Revenue.create(:name => "Services Revenue")
@@ -16,37 +22,44 @@ Plutus::Expense.create(:name => "Auto expense")
 Plutus::Liability.create(:name => "Sales Tax Payable")
 Plutus::Liability.create(:name => "Credit Card Payable")
 
-Plutus::Entry.destroy_all
-Plutus::Amount.destroy_all
+FinanceInstitution.create(:plaid_type => "amex", :name => "American Express")
+FinanceInstitution.create(:plaid_type => "bofa", :name => "Bank of America")
+FinanceInstitution.create(:plaid_type => "chase", :name => "Chase")
+FinanceInstitution.create(:plaid_type => "citi", :name => "Citi")
+FinanceInstitution.create(:plaid_type => "us", :name => "US Bank")
+FinanceInstitution.create(:plaid_type => "USAA", :name => "USAA")
+FinanceInstitution.create(:plaid_type => "wells", :name => "Wells Fargo")
 
-entry1 = Plutus::Entry.build(
-                :description => "Gasoline",
-                :debits => [
-                  {:account => "Auth expense", :amount => 100.00}],
-                :credits => [
-                  {:account => "Cash", :amount => 100.00}])
-                  entry1.save
+if Rails.env == "development"
+  entry1 = Plutus::Entry.build(
+                  :description => "Gasoline",
+                  :debits => [
+                    {:account => "Auth expense", :amount => 100.00}],
+                  :credits => [
+                    {:account => "Cash", :amount => 100.00}])
+                    entry1.save
 
-entry2 = Plutus::Entry.build(
-                :description => "Editing expense",
-                :debits => [
-                  {:account => "Payroll expense", :amount => 25.00}],
-                :credits => [
-                  {:account => "Cash", :amount => 25.00}])
-                  entry2.save
+  entry2 = Plutus::Entry.build(
+                  :description => "Editing expense",
+                  :debits => [
+                    {:account => "Payroll expense", :amount => 25.00}],
+                  :credits => [
+                    {:account => "Cash", :amount => 25.00}])
+                    entry2.save
 
-entry3 = Plutus::Entry.build(
-                :description => "Took Dan Fogal out to lunch",
-                :debits => [
-                  {:account => "Food and Entertainment Expense", :amount => 38.25}],
-                :credits => [
-                  {:account => "Credit Card Payable", :amount => 38.25}])
-                  entry3.save
+  entry3 = Plutus::Entry.build(
+                  :description => "Took Dan Fogal out to lunch",
+                  :debits => [
+                    {:account => "Food and Entertainment Expense", :amount => 38.25}],
+                  :credits => [
+                    {:account => "Credit Card Payable", :amount => 38.25}])
+                    entry3.save
 
-entry4 = Plutus::Entry.build(
-                :description => "Family wedding pictures",
-                :debits => [
-                  {:account => "Cash", :amount => 1000}],
-                :credits => [
-                  {:account => "Services Revenue", :amount => 1000}])
-                  entry4.save
+  entry4 = Plutus::Entry.build(
+                  :description => "Family wedding pictures",
+                  :debits => [
+                    {:account => "Cash", :amount => 1000}],
+                  :credits => [
+                    {:account => "Services Revenue", :amount => 1000}])
+                    entry4.save
+  end
