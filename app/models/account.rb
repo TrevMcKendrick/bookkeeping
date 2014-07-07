24e0c594b0a6a2e94fb28a5f6439d0275134fcfb
trevmckendrick <trevormckendrick@gmail.com>
2014-07-06 22:27:52 -0600

    to_s added to start_day and end_day

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,8 +4,6 @@ class Account < ActiveRecord::Base
   has_many :debits
   has_many :credits
 
-  DEFAULTS = [["Cash", "Asset"], ["Accounts Receivable", "Asset"], ["Equipment", "Asset"], ["Accounts Payable", "Liability"], ["Payroll Expense", "Expense"], ["Sales", "Revenue"]]
-
   # validates_uniqueness_of :name
   validates_presence_of :type, :name
 
@@ -20,12 +18,12 @@ class Account < ActiveRecord::Base
   end
 
   def debit_balance_between_dates(start_day, end_day)
-    time_range = (start_day)..(end_day)
+    time_range = (start_day.to_s)..(end_day.to_s)
     Debit.joins(:journal_entry, account: :user).where('journal_entries.effective_date' => time_range, 'accounts.name' => self.name, 'users.id' => self.user.id).sum(:amount)
   end
 
   def credit_balance_between_dates(start_day, end_day)
-    time_range = (start_day)..(end_day)
+    time_range = (start_day.to_s)..(end_day.to_s)
     Credit.joins(:journal_entry, account: :user).where('journal_entries.effective_date' => time_range, 'accounts.name' => self.name, 'users.id' => self.user.id).sum(:amount)
   end
 
@@ -51,10 +49,6 @@ class Account < ActiveRecord::Base
     total = self.all.collect do |account|
       account.credit_balance_between_dates(start_day, end_day)
     end.sum
-  end
-
-  def trial_balance
-    debit_balance_as_of(Date.today) - credit_balance_as_of(Date.today)
   end
 
 end
