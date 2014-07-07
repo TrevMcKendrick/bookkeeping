bb99097cba93c679fa9e46c4750c4e83c84338f8
trevmckendrick <trevormckendrick@gmail.com>
2014-07-06 19:59:19 -0600

    JE validations

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,7 +1,10 @@
 class JournalEntry < ActiveRecord::Base
   has_many :debits
   has_many :credits
-  # belongs_to :user
+
+  validate :has_debits?
+  validate :has_credits?
+  validate :balances?
 
   def debit_total
     self.debits.sum(:amount)
@@ -10,4 +13,19 @@ class JournalEntry < ActiveRecord::Base
   def credit_total
     self.credits.sum(:amount)
   end
+
+  private
+
+  def has_debits?
+    errors[:base] << "Journal entry needs a debit" if self.debits.blank?
+  end
+
+  def has_credits?
+    errors[:base] << "Journal entry needs a credit" if self.credits.blank?
+  end
+
+  def balances?
+    errors[:base] << "The journal entry doesn't balance" if self.debit_total != self.credit_total
+  end
+
 end
