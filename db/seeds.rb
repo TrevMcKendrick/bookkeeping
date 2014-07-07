2a6a382ca0887dc5fd06b1debf3d20d33abc5567
trevmckendrick <trevormckendrick@gmail.com>
2014-07-06 22:40:46 -0600

    Added seed entries for 2nd test user (Breanna)

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,25 @@ trevor = User.create(:email => "trevor@gmail.com", :password => "test")
 breanna = User.create(:email => "breanna@gmail.com", :password => "test")
 
 #pair
-je = EntryBuilder.new(:amount => 225, :date => (Date.today - 6.months + 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(trevor)
-je = EntryBuilder.new(:amount => 225, :date => (Date.today - 6.months + 2.weeks), :debit_account => "Accounts Payable", :credit_account => "Cash" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 300, :date => (Date.today - 6.months + 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 300, :date => (Date.today - 6.months + 2.weeks), :debit_account => "Accounts Payable", :credit_account => "Cash" ).as_entry(trevor)
 #pair
-je = EntryBuilder.new(:amount => 150, :date => (Date.today - 1.month), :debit_account => "Accounts Receivable", :credit_account => "Sales" ).as_entry(trevor)
-je = EntryBuilder.new(:amount => 150, :date => (Date.today - 1.month), :debit_account => "Cash", :credit_account => "Accounts Receivable" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 100, :date => (Date.today - 1.month), :debit_account => "Accounts Receivable", :credit_account => "Sales" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 100, :date => (Date.today - 1.month), :debit_account => "Cash", :credit_account => "Accounts Receivable" ).as_entry(trevor)
 
-je = EntryBuilder.new(:amount => 100, :date => (Date.today), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(trevor)
-je = EntryBuilder.new(:amount => 125, :date => (Date.today - 5.days), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(trevor)
-je = EntryBuilder.new(:amount => 525, :date => (Date.today - 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(trevor)
-je = EntryBuilder.new(:amount => 1000, :date => (Date.today - 1.year), :debit_account => "Cash", :credit_account => "Capital Contributions" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 500, :date => (Date.today), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 300, :date => (Date.today - 5.days), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 700, :date => (Date.today - 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(trevor)
+je = EntryBuilder.new(:amount => 2500, :date => (Date.today - 1.year), :debit_account => "Cash", :credit_account => "Capital Contributions" ).as_entry(trevor)
+
+#pair
+je = EntryBuilder.new(:amount => 3, :date => (Date.today - 6.months + 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(breanna)
+je = EntryBuilder.new(:amount => 3, :date => (Date.today - 6.months + 2.weeks), :debit_account => "Accounts Payable", :credit_account => "Cash" ).as_entry(breanna)
+#pair
+je = EntryBuilder.new(:amount => 1, :date => (Date.today - 1.month), :debit_account => "Accounts Receivable", :credit_account => "Sales" ).as_entry(breanna)
+je = EntryBuilder.new(:amount => 1, :date => (Date.today - 1.month), :debit_account => "Cash", :credit_account => "Accounts Receivable" ).as_entry(breanna)
+
+je = EntryBuilder.new(:amount => 5, :date => (Date.today), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(breanna)
+je = EntryBuilder.new(:amount => 3, :date => (Date.today - 5.days), :debit_account => "Cash", :credit_account => "Sales" ).as_entry(breanna)
+je = EntryBuilder.new(:amount => 7, :date => (Date.today - 1.day), :debit_account => "Payroll Expense", :credit_account => "Accounts Payable" ).as_entry(breanna)
+je = EntryBuilder.new(:amount => 25, :date => (Date.today - 1.year), :debit_account => "Cash", :credit_account => "Capital Contributions" ).as_entry(breanna)
